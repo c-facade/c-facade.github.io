13c8a650659863ff71320c06cb0428260ae4f289
c-facade <francecipinna@gmail.com>
2025-04-30 12:17:10 +0200

    handle caching

--- docs/app/immutable/entry/start.DWPHixqv.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/D9_woEna.js";export{t as start};
--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -7,28 +7,80 @@
 	export let image : string = "rick.gif"
 	export let imageAlt : string = "cool image or gif";
 	export let repo : string = "Anki2Notes"
+	let repotime : string = repo+"_TIMESTAMP";
 	const endpoint = "https://api.github.com/repos/c-facade/" + repo;
-	let lastUpdate : Date = new Date(2025);
-	onMount(async function () {
+	let lastUpdate : Date = new Date(2025, 1);
+
+	async function updateRepo(){
 		let data;
-		if(!(repo in localStorage)){
+		let valid = false;
+		if(repotime in localStorage){
+			let timestamp : number = Number(localStorage.getItem(repotime));
+			if(Date.now() - timestamp < 3600*1000){
+				//console.log("now - timestamp = ", Date.now()-timestamp);
+				valid = true;
+			}
+		}
+		if(!valid){	
+			// fetching
+			//console.log("Fetching again");
 			const response = await fetch(endpoint);
 			if(!response.ok) {
 				throw new Error(`Response status: ${response.status ?? "error"}`);
 			}
 			else{
+				// no error, storing response
 				data = await response.json();
 				console.debug(JSON.stringify(data));
 				localStorage.setItem(repo, JSON.stringify(data));
+				localStorage.setItem(repotime, Date.now().toString());
 			}
 		}
-		else{
+		else{	
+			//console.log("valid: not fetching again");
+			// cache value is still valid
 			var jsons = localStorage.getItem(repo);
 			if(jsons != null){
 				console.debug(JSON.stringify(jsons));
 				data = JSON.parse(jsons);
 			}
 		}
+		return data;
+}
+
+	onMount(async function () {
+		let data = await updateRepo();
+
+/*
+		if(!(repo in localStorage)){
+			// fetching for the first time
+			const response = await fetch(endpoint);
+			if(!response.ok) {
+				throw new Error(`Response status: ${response.status ?? "error"}`);
+			}
+			else{
+				// no error, storing response
+				data = await response.json();
+				console.debug(JSON.stringify(data));
+				localStorage.setItem(repo, JSON.stringify(data));
+				localStorage.setItem(repotime, Date.now().toString());
+			}
+		}
+		else{
+			let timestamp : number = Number(localStorage.getItem(repotime));
+			if(Date.now() - 100000 > timestamp){
+			}
+			else{
+				// cache value is still valid
+				var jsons = localStorage.getItem(repo);
+				if(jsons != null){
+					console.debug(JSON.stringify(jsons));
+					data = JSON.parse(jsons);
+				}
+			}
+		}
+		*/
+
 		link = data.html_url;
 		description = data.description;
 		lastUpdate = new Date(data.updated_at);
