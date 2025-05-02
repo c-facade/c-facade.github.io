4ea34dfe1539bbbd3d155fac10049ddb022550cd
c-facade <francecipinna@gmail.com>
2025-05-02 13:15:19 +0200

    add to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,10 +14,19 @@
 
 <ul>
 	<li>I'm a graduate student in computer science at the University of Pisa.</li>
-	<li>I have a Bachelor's degree in computer science with grade 104. 
-	</li>
-	<li>My Bachelor's thesis in italian: <a href="Tesi_Francesca_Pinna.pdf">Sviluppo di un driver Linux per gestione
-			energetica in un gateway edge</a></li>
-	<li>I'm mostly interested in low level programming, but I can also work with web applications.</li>
+	<li>I have a Bachelor's degree in computer science with grade 104/110.
+		</li>
+		<li>My thesis in italian: <a href="Tesi_Francesca_Pinna.pdf">Sviluppo di un driver Linux per gestione
+			energetica in un gateway edge</a> (Developing a Linux driver for power management in an edge gateway).</li>
+		<li>I'm mostly interested in low level programming, but I can also work with web applications.
+			I plan on working in the IoT field.
+		</li>
+		<li>
+			I'm also into computer archeology and retro-programming. I'm a guide at the <a href="https://www.msc.sma.unipi.it/">Museum of Computing Machinery</a> of Pisa.
+		</li>
 </ul>
+<figure>
+	<img style="max-width: 100%" src="image/mostra-hello-world.jpg" alt="mostra hello world illustration"/>
+	<figcaption>Hello World Exhibit</figcaption>
+</figure>
 </main>
